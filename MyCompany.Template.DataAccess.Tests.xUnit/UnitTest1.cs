1710480607d616009bbf013d876e94753dc4aff6
Tom <tomb@adeng.co.il>
2023-07-10 16:24:03 +0300

    Added xUnit empty tests

---
+++ MyCompany.Template.DataAccess.Tests.xUnit/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+using Xunit;
+
+namespace MyCompany.Template.DataAccess.Tests.xUnit
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ MyCompany.Template.UI.Tests.xUnit/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+using Xunit;
+
+namespace MyCompany.Template.UI.Tests.xUnit
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void TestMethod1()
+        {
+        }
+    }
+}
